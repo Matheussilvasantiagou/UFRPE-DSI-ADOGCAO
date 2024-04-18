ea40b143f9a078ab4dfa75631a3a960c55423398
Guilherme de Moraes <gmsgui1000@gmail.com>
2024-04-17 23:27:10 -0300

    Refactor Login page

--- adogcao/lib/pages/login_page.dart
+++ adogcao/lib/pages/login_page.dart
@@ -1,9 +1,10 @@
 import 'package:adogcao/components/my_button.dart';
-import 'package:adogcao/components/square_title.dart';
 import 'package:flutter/material.dart';
 import 'package:adogcao/components/my_textfield.dart';
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -17,232 +18,100 @@ class _LoginPageState extends State<LoginPage> {
   void signUserIn() {
     setState(() {
       _validatePassword = passwordController.text.isEmpty;
-      final bool validateUsuarioFormat = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-      .hasMatch(passwordController.text);
-      _validateUsuario = (usernameController.text.isEmpty) && !validateUsuarioFormat;
+      final bool validateUsuarioFormat = RegExp(
+              r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+          .hasMatch(passwordController.text);
+      _validateUsuario =
+          (usernameController.text.isEmpty) && !validateUsuarioFormat;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-
-              Image.asset(
-                'lib/images/logo.jpeg',
-                width: 150,
-                height: 150,
-              ),
-
-              const SizedBox(height: 50),
-
-              const SizedBox(height: 50),
-
-              Text(
-                'Bem Vindo',
-                style: TextStyle(
-                  color: Colors.grey[700],
-                  fontSize: 16,
-
-                ),
-              ),
-              const SizedBox(height: 25),
-              MyTextField(
-                controller: usernameController,
-                hintText: 'Email',
-                obscureText: false,
-                errorText: _validateUsuario ? "Campo usuário inválido" : null,
-              ),
-              const SizedBox(height: 10),
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Senha',
-                obscureText: true,
-                errorText: _validatePassword ? "Campo senha é obrigatório" : null,
-              ),
-              const SizedBox(height: 10),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Esqueceu a senha?',
-                        style: TextStyle(color: Colors.grey[600]),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 25),
-
-                // sign in button
-                MyButton(
-                  onTap: signUserIn
-                ),
-
-                
-                const SizedBox(height: 50),
-
-                // or continue with
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                        child: Text(
-                          'Ou entre com',
-                          style: TextStyle(color: Colors.grey[700]),
-                        ),
-                      ),
-                      Expanded(
-                        child: Divider(
-                          thickness: 0.5,
-                          color: Colors.grey[400],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 50),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    // google button
-                    SquareTile(imagePath: 'lib/images/google.png'),
-                  ],
-                ),
-
-                const SizedBox(height: 50),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-              ),
-
-              const SizedBox(height: 25),
-
-              MyTextField(
-                controller: usernameController,
-                hintText: 'Email',
-                obscureText: false,
-              ),
-
-              const SizedBox(height: 10),
-
-              MyTextField(
-                controller: passwordController,
-                hintText: 'Senha',
-                obscureText: true,
-              ),
-
-              const SizedBox(height: 10),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Text(
-                      'Esqueceu a senha?',
-                      style: TextStyle(color: Colors.grey[600]),
-                    ),
-                  ],
-                ),
-              ),
-
-              const SizedBox(height: 25),
-
-              // sign in button
-              MyButton(
-                onTap: signUserIn,
-              ),
-
-              const SizedBox(height: 50),
-
-              // or continue with
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Text(
-                        'Ou entre com',
-                        style: TextStyle(color: Colors.grey[700]),
-                      ),
-                    ),
-                    Expanded(
-                      child: Divider(
-                        thickness: 0.5,
-                        color: Colors.grey[400],
-                      ),
-                    ),
-                  ],
-
-                )
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  // google button
-                  SquareTile(imagePath: 'lib/images/google.png'),
-                ],
-              ),
-
-              const SizedBox(height: 50),
-
-              Row(
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment(6.123234262925839e-17, 1),
+              end: Alignment(-1, 6.123234262925839e-17),
+              colors: [
+                Color.fromRGBO(22, 22, 24, 1),
+                Color.fromRGBO(30, 38, 51, 1)
+              ]),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    'Não tem cadastro?',
-                    style: TextStyle(color: Colors.grey[700]),
+                  const SizedBox(height: 50),
+                  Image.asset(
+                    'lib/images/logo.jpeg',
+                    width: 150,
+                    height: 150,
                   ),
-                  const SizedBox(width: 4),
+
+                  const SizedBox(height: 50),
+
                   const Text(
-                    'Cadastre-se agora',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  'Entrar',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
                   ),
+                ),
+
+                  const SizedBox(height: 25),
+
+                  MyTextField(
+                    controller: usernameController,
+                    hintText: 'Email',
+                    obscureText: false,
+                    errorText: _validateUsuario ? "Campo email inválido" : null,
+                  ),
+
+                  const SizedBox(height: 10),
+
+                  MyTextField(
+                    controller: passwordController,
+                    hintText: 'Senha',
+                    obscureText: true,
+                    errorText:
+                        _validatePassword ? "Campo senha é obrigatório" : null,
+                  ),
+
+                  const SizedBox(height: 260),
+
+                  MyButton(onTap: signUserIn),
+
+                  const SizedBox(height: 25),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Não tem cadastro?',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
+                      const SizedBox(width: 4),
+                      const Text(
+                        'Cadastre-se agora',
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
 
 void main() {
-  runApp(MaterialApp(home: LoginPage()));
+  runApp(const MaterialApp(home: LoginPage()));
 }
