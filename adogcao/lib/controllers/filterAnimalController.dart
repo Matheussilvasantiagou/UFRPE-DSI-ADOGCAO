c0076d643b438c77d64bd2b8ad1cc72e8cb2dcd8
EstomJr <estonjr2@hotmail.com>
2024-09-24 21:17:44 -0300

    fix: funções para filtros por tipo de categorias e filtros de por todos os dados dos animais

--- adogcao/lib/controllers/filterAnimalController.dart
+++ adogcao/lib/controllers/filterAnimalController.dart
@@ -1,25 +1,64 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_application_1/models/animal.dart';
 
-class FilterAnimalController{
+class FilterAnimalController {
+  Future<List<Animal>> getFilteredAnimals(
+      String nome, String tipo, String abrigo) async {
+    Query query = FirebaseFirestore.instance.collection('pets');
 
-  Future<List<Animal>> getFilteredAnimals(String nome, String tipo, String abrigo) async{
-  Query query = FirebaseFirestore.instance.collection('pets');
+    if (nome.isNotEmpty) {
+      query = query.where('name', isEqualTo: nome);
+    }
+    if (tipo.isNotEmpty) {
+      query = query.where('animalType', isEqualTo: tipo);
+    }
+    if (abrigo.isNotEmpty) {
+      query = query.where('shelterId', isEqualTo: abrigo);
+    }
 
-  if (nome.isNotEmpty) {
-    query = query.where('name', isEqualTo: nome);
-  }
-  if (tipo.isNotEmpty) {
-    query = query.where('animalType', isEqualTo: tipo);
-  }
-  if (abrigo.isNotEmpty) {
-    query = query.where('shelterId', isEqualTo: abrigo);
-  }
-
-  QuerySnapshot querySnapshot = await query.get();
-  return querySnapshot.docs.map((doc) => Animal.fromDocument(doc)).toList();
+    QuerySnapshot querySnapshot = await query.get();
+    return querySnapshot.docs.map((doc) => Animal.fromDocument(doc)).toList();
   }
 
+  // Função específica para filtrar apenas por categoria (tipo de animal)
+  Future<List<Animal>> getFilteredAnimalsByCategory(String category) async {
+    Query query = FirebaseFirestore.instance.collection('pets');
+
+    if (category.isNotEmpty) {
+      query = query.where('animalType', isEqualTo: category);
+    }
+
+    QuerySnapshot querySnapshot = await query.get();
+    return querySnapshot.docs.map((doc) => Animal.fromDocument(doc)).toList();
+  }
+
+  // Função para buscar todas as opções de abrigos
+  Future<List<String>> getShelters() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('pets').get();
+    return querySnapshot.docs
+        .map((doc) => doc['shelterId'] as String)
+        .toSet()
+        .toList();
+  }
+
+  // Função para buscar todas as opções de idades dos animais
+  Future<List<String>> getAges() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('pets').get();
+    return querySnapshot.docs
+        .map((doc) => doc['age'] as String)
+        .toSet()
+        .toList(); // Elimina duplicatas com .toSet()
+  }
+
+  // Função para buscar todas as opções de pesos dos animais
+  Future<List<String>> getWeights() async {
+    QuerySnapshot querySnapshot =
+        await FirebaseFirestore.instance.collection('pets').get();
+    return querySnapshot.docs
+        .map((doc) => doc['weight'] as String)
+        .toSet()
+        .toList(); // Elimina duplicatas com .toSet()
+  }
 }
-
-
